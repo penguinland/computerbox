b7bd8635c43013ce6e31e78ee2d40db73b9619b9
Alan <alan.davidson@gmail.com>
2012-11-25 00:40:30 -0500

    adding the TTS module

--- computerbox/configuration.py
+++ computerbox/configuration.py
@@ -1,6 +1,6 @@
 """
-This file is for constants that get used in lots of places. This way, there's
-only one file you need to modify.
+This file is for constants that get used in other places. This way, there's only
+one file you need to modify.
 """
 
 # The path to the directory containing the .fsg files for speech recognition.
@@ -8,3 +8,25 @@ only one file you need to modify.
 # a quirk in PocketSphinx that I haven't worked out yet, it appears that this
 # needs to be fully specified; you can't use a tilde for your home directory.
 DATA_DIR = "/Users/alandavidson/computerbox/computerbox/data"
+
+# A mapping from mispronounced lowercased words to phonetic versions. Comments
+# are phonetic versions of the default pronunciation, to explain why the word
+# was added here.
+PRONUNCIATION_CORRECTIONS = {
+  "edt" : "eastern time",  # ee dee tee
+  "fla" : "Florida",  # flah
+  "gif" : "giff",  # Gee Ai Eff
+  "gui" : "gooey",  # Gee You Ai
+  "qty" : "quantity",  # tai
+  "mpg" : "miles per gallon",  # em pee gee
+  "mph" : "miles per hour",  # em pee aitch
+  "mr" : "mister",  # em arr
+  "ms" : "miss",  # em ess
+  "mrs" : "misses",  # em arr es
+  "jr" : "junior",  # jay arr
+  "sr" : "senior",  # ess arr
+  "sen" : "senator",  # sen
+  "syria" : "seerya", # surria
+  "sc" : "south carolina",  # ess see
+  }
+
