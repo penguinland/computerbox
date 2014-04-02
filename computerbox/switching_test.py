8f12f8879c84986d5d8deaa49f121d4c2917781f
Alan <alan.davidson@gmail.com>
2014-04-01 21:56:41 -0400

    Getting switching demo working

--- computerbox/switching_test.py
+++ computerbox/switching_test.py
@@ -1,25 +1,15 @@
 #!/usr/bin/python
 
-"""
-If you run this with no arguments from the command line, it can recognize the
-grammar
-  (ONE|TWO|THREE) (STOP|END)
-When you are done playing around with it, use control-C to quit.
-
-If you run this with an argument, it will use the dictionary and FSG named by
-that argument in the data directory. For example, running 
-  demo_test.py test
-is equivalent to running this with no arguments.
-"""
+# Say "THREE END" to switch to the news commands, and say "STOP READING
+# HEADLINES" to switch back to the test commands.
 
 import gobject
 import pygst
-pygst.require('0.10')
+pygst.require("0.10")
 gobject.threads_init()
 import gst
 
 import signal
-import sys
 import time
 
 import configuration
@@ -30,24 +20,25 @@ import configuration
 # the other files in this directory.
 
 pipeline = gst.parse_launch(
-  "gconfaudiosrc ! audioconvert ! audioresample ! " +
+  #"gconfaudiosrc ! " +
+  ("pulsesrc device=%s ! " % configuration.PULSESRC_NAME) +
+  "audioconvert ! audioresample ! " +
   "vader name=vad auto-threshold=true ! pocketsphinx name=asr ! " +
   "fakesink")
 
 def SwitchTo(name):
   print "Switching FSG and dictionary to %s..." % name
   filename = "%s/%s" % (configuration.DATA_DIR, name)
-  asr = pipeline.get_by_name('asr')
+  asr = pipeline.get_by_name("asr")
   pipeline.set_state(gst.STATE_PAUSED)
-  asr.set_property('configured', False)
-  asr.set_property('fsg', "%s.fsg" % filename)
-  asr.set_property('dict', "%s.dic" % filename)
-  asr.set_property('configured', True)
+  asr.set_property("configured", False)
+  asr.set_property("fsg", "%s.fsg" % filename)
+  asr.set_property("dict", "%s.dic" % filename)
+  asr.set_property("configured", True)
   pipeline.set_state(gst.STATE_PLAYING)
   print "done switching!"
 
 def AsrResult(asr, text, uttid):
-  """Forward result signals on the bus to the main thread."""
   print "\nGot result: %s\n" % text
   #time.sleep(1)
   print "resuming..."
@@ -57,24 +48,18 @@ def AsrResult(asr, text, uttid):
     SwitchTo("news")
 
 def AsrPartialResult(asr, text, uttid):
-  """Forward result signals on the bus to the main thread."""
   print "\nGot partial result: %s\n" % text
 
 def Initialize():
   """Initialize the speech components"""
-  if len(sys.argv) > 1:
-    file = sys.argv[1]
-  else:
-    file = "test"
-  filename = "%s/%s" % (configuration.DATA_DIR, file)
+  filename = "%s/test" % configuration.DATA_DIR
 
-  asr = pipeline.get_by_name('asr')
-  asr.set_property('fsg', "%s.fsg" % filename)
-  asr.set_property('dict', "%s.dic" % filename)
-  asr.connect('result', AsrResult)
-  #asr.connect('partial_result', AsrPartialResult)
-  asr.set_property('configured', True)
-
+  asr = pipeline.get_by_name("asr")
+  asr.set_property("fsg", "%s.fsg" % filename)
+  asr.set_property("dict", "%s.dic" % filename)
+  asr.connect("result", AsrResult)
+  #asr.connect("partial_result", AsrPartialResult)
+  asr.set_property("configured", True)
   pipeline.set_state(gst.STATE_PLAYING)
 
 Initialize()
